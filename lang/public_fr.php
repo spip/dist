a7f0994049d03675d790c021cc9f5019676b8ae2
Jacques Pyrat <jacques@pyrat.net>
2020-06-02 22:37:34 +0200

    Feuille de style XSLT (inspirée de celle de DotClear) permettant d'afficher le contenu du flux RSS précédé d'une explication didactique de ce à quoi ça sert et comment s'en servir dans un agrégateur.

---
+++ lang/public_fr.php
@@ -0,0 +1,15 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// Fichier source, a modifier dans https://git.spip.net/spip/dist.git
+if (!defined('_ECRIRE_INC_VERSION')) {
+	return;
+}
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	'rss_abonnement' => 'Copiez l’URL suivante dans votre agrégateur :',
+	'rss_abonnement_titre' => 'S’abonner',
+	'rss_abonnement_titre_page' => 'S’abonner :',
+	'rss_explication' => 'Un flux RSS est un résumé automatique et gratuit des actualités d’un site. Il fournit le contenu ou un extrait du contenu, accompagné de liens vers le contenu complet. Les dernières actualités publiées peuvent être lues dans votre <a href="https://fr.wikipedia.org/wiki/Agr%C3%A9gateur">agrégateur RSS</a> de prédilection.',
+	'rss_explication_titre' => 'Qu’est-ce qu’un flux RSS ?'
+);
